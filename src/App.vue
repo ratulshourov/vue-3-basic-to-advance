1ee1c9027a748e57f975f97f48cc6f609331ec16
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2023-02-15 10:03:20 +0600

    fresh project vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    {{name}}
+    <h1 v-text="phone"></h1>
+    <div v-html="title"></div>
+    <h2 v-bind:class="[flag&&'stock','stockout']">SLOCK LIST /SLOCK OUT  BY ARRAY CLASS</h2>
+    <h2 v-bind:class="{
+      isActive,
+      flag:!newCustomer,'active':'notActive'
+    }">SLOCK LIST /SLOCK OUT  BY Object CLASS</h2>
+    <button v-bind:disabled="disableButton">Add</button>
+    <h1 v-bind:id="idname">Heading</h1>
+    <h1 v-bind:class="disableButton && 'notActive'">Active Class</h1>
+    <h1 v-bind:class="flag ? 'active':'notActive'">Flag Check</h1>
+    
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    
+  },
+  data() {
+    return {
+      name: 'ratul',
+      phone:12344,
+      title:`<h1>PRACTICE</h1>`,
+      disableButton:true,
+      idname:'main',
+      flag:false,
+      isActive:false,
+      newCustomer:true
+    };
+  },
 }
 </script>
 
@@ -22,5 +49,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.active {
+  color: green;
+}
+.notActive {
+  color: red;
+}
+
+.stock {
+  font-style: italic;
+}
+
+.stockout{
+  background: yellow;
 }
 </style>
